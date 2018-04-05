b864981102bda5642d95a818ecfd0e504872dfbc
Nick Brain <NicklausBrain@hotmail.com>
2018-04-05 18:45:43 +0300

    removing unnecessary files

--- Vet.Clinic.Mvc/App_Start/RouteConfig.cs
+++ Vet.Clinic.Mvc/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace VetClinic.Mvc
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Patients", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
